3a53376e2a7edd5b1b0d5ce68fbcda10a9b9a008
Max-Mackey <maxmackey@gmail.com>
2023-03-28 11:07:44 -0700

    Submit button creates new divs via DOM manip with info of a new book. Clear data from forms once button press. Need to polish layout next

--- code.js
+++ code.js
@@ -12,12 +12,52 @@ closeButton.onclick = function () {
   overlay.style.display = "none";
 };
 
-let myLibrary = [];
-function Book() {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
+//submit button adds all data to a new book and stores within a library array
+const submitBtn = document.querySelector("#submitBtn");
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  //pulls data from form inputs to add to object
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
 
-console.log(title);
+  function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+  }
+  let myLibrary = [];
+  // adds new book to library array
+  const book = new Book(title, author, pages);
+  myLibrary.push(book);
+  const bookIndex = 0;
+  console.log(myLibrary[bookIndex].title);
+  const newBook = document.createElement("div");
+  newBook.classList.add("book");
+  //adds a title to the newBook div
+  const newTitle = document.createElement("div");
+  newTitle.textContent = `"${book.title}"`;
+  newBook.appendChild(newTitle);
+  newBook.classList.add("content");
+  //adds an author to the newBook div
+  const newAuthor = document.createElement("div");
+  newAuthor.textContent = book.author;
+  newBook.appendChild(newAuthor);
+  newBook.classList.add("content");
+  //adds the pages to the newBook div
+  const newPages = document.createElement("div");
+  newPages.textContent = `${book.pages} pages`;
+  newBook.appendChild(newPages);
+  newBook.classList.add("content");
+  //adds the new book to the centered book div on web page
+  const booksDiv = document.querySelector(".books");
+  booksDiv.appendChild(newBook);
+
+  // closes form box and returns page to normal state
+  popup.style.display = "none";
+  overlay.style.display = "none";
+  // clears form fields for next submission
+  document.querySelector("#title").value = "";
+  document.querySelector("#author").value = "";
+  document.querySelector("#pages").value = "";
+});
